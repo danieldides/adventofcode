6043fb60fdac72e57d9c071fb554b32aff6ed35f
Daniel Dides <senorequeso@gmail.com>
2019-12-22 18:05:24 -0600

    Calculate minimum amount of steps

--- 2019/03/main.go
+++ 2019/03/main.go
@@ -18,14 +18,15 @@ func main() {
 	parts := strings.Split(string(file), "\n")
 	wire1 := strings.Split(parts[0], ",")
 	wire2 := strings.Split(parts[1], ",")
-	ret := Compute(wire1, wire2)
-	log.Printf("Part one: %v\n", ret)
+	distance, steps := Compute(wire1, wire2)
+	log.Printf("Part one: %v\n", distance)
+	log.Printf("Part two: %v\n", steps)
 }
 
 // Compute takes in the input and gives us the processed output
-func Compute(wire1, wire2 []string) int {
-	visited1 := drive(wire1)
-	visited2 := drive(wire2)
+func Compute(wire1, wire2 []string) (int, int) {
+	visited1, m1 := drive(wire1)
+	visited2, m2 := drive(wire2)
 
 	intersection := intersect.Hash(visited1, visited2).([]interface{})
 
@@ -34,6 +35,11 @@ func Compute(wire1, wire2 []string) int {
 		points = append(points, point.(loc))
 	}
 
+	if len(points) == 0 {
+		log.Fatal("Empty intersections")
+	}
+
+	// Find the shortest distance
 	var sums []int
 	for _, p := range points {
 		sum := abs(p.x) + abs(p.y)
@@ -47,7 +53,16 @@ func Compute(wire1, wire2 []string) int {
 		}
 	}
 
-	return min
+	// Calculate the minimum steps at all intersections
+	minSteps := m1[points[0]] + m1[points[1]]
+	for _, point := range points {
+		steps := m1[point] + m2[point]
+		if steps < minSteps {
+			minSteps = steps
+		}
+	}
+
+	return min, minSteps
 }
 
 type loc struct {
@@ -56,45 +71,56 @@ type loc struct {
 }
 
 // Drive every point in path and return a list of all the X/Y coordinates
-func drive(path []string) []loc {
-
+func drive(path []string) ([]loc, map[loc]int) {
 	x := 0
 	y := 0
+	steps := 0
 
 	var visited []loc
 
-	for _, point := range path {
+	// Map a location to a numeber of steps
+	m := make(map[loc]int)
 
+	for _, point := range path {
 		cmd := string(point[0])
 		rest := string(point[1:])
 		distance, _ := strconv.Atoi(rest)
-
 		switch cmd {
 		case "U":
 			for i := 0; i < distance; i++ {
+				steps++
 				y++
-				visited = append(visited, loc{x, y})
+				l := loc{x, y}
+				visited = append(visited, l)
+				m[l] = steps
 			}
 		case "R":
 			for i := 0; i < distance; i++ {
+				steps++
 				x++
-				visited = append(visited, loc{x, y})
+				l := loc{x, y}
+				visited = append(visited, l)
+				m[l] = steps
 			}
 		case "D":
 			for i := 0; i < distance; i++ {
+				steps++
 				y--
-				visited = append(visited, loc{x, y})
+				l := loc{x, y}
+				visited = append(visited, l)
+				m[l] = steps
 			}
 		case "L":
 			for i := 0; i < distance; i++ {
+				steps++
 				x--
-				visited = append(visited, loc{x, y})
+				l := loc{x, y}
+				visited = append(visited, l)
+				m[l] = steps
 			}
-
 		}
-		visited = append(visited, loc{x, y})
 	}
-	return visited
+	return visited, m
 }
 
 func abs(x int) int {
--- 2019/03/main_test.go
+++ 2019/03/main_test.go
@@ -7,32 +7,39 @@ import (
 func TestCompute(t *testing.T) {
 
 	tests := []struct {
-		wire1 []string
-		wire2 []string
-		out   int
+		wire1    []string
+		wire2    []string
+		distance int
+		steps    int
 	}{
 		{
-			wire1: []string{"R8", "U5", "L5", "D3"},
-			wire2: []string{"U7", "R6", "D4", "L7"},
-			out:   6,
+			wire1:    []string{"R8", "U5", "L5", "D3"},
+			wire2:    []string{"U7", "R6", "D4", "L7"},
+			distance: 6,
+			steps:    30,
 		},
 
 		{
-			wire1: []string{"R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"},
-			wire2: []string{"U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"},
-			out:   159,
+			wire1:    []string{"R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"},
+			wire2:    []string{"U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"},
+			distance: 159,
+			steps:    610,
 		},
 		{
-			wire1: []string{"R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"},
-			wire2: []string{"U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"},
-			out:   135,
+			wire1:    []string{"R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"},
+			wire2:    []string{"U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"},
+			distance: 135,
+			steps:    410,
 		},
 	}
 
 	for i, test := range tests {
-		out := Compute(test.wire1, test.wire2)
-		if out != test.out {
-			t.Errorf("Fail! Input: %v Expected: %v Got: %v", i+1, test.out, out)
+		distance, steps := Compute(test.wire1, test.wire2)
+		if distance != test.distance {
+			t.Errorf("Distance Fail! Input: %v Expected: %v Got: %v", i+1, test.distance, distance)
+		}
+		if steps != test.steps {
+			t.Errorf("Steps Fail! Input: %v Expected: %v Got: %v", i+1, test.steps, steps)
 		}
 	}
 }
