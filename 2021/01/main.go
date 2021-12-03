85b85927e6df6aa23ac0a67901785150220a2cab
Daniel Dides <danieldides@gmail.com>
2021-12-02 22:23:48 -0600

    1.2

--- 2021/01/main.go
+++ 2021/01/main.go
@@ -27,17 +27,38 @@ func main() {
 	}
 
 	log.Printf("Part 1: %v\n", DoOne(in))
+	log.Printf("Part 2: %v\n", DoTwo(in))
 
+}
+
+func countIncreases(in []int) int {
+	var result int
+
+	for i := 1; i < len(in); i++ {
+		// fmt.Print(in[i])
+		if in[i] > in[i-1] {
+			// fmt.Print(" Increased\n")
+			result++
+			continue
+		}
+		// fmt.Print(" Decreased\n")
+	}
+	return result
 }
 
 // Part 1
 func DoOne(in []int) int {
-	var result int
+	return countIncreases(in)
+}
 
-	for i := 1; i < len(in); i++ {
-		if in[i] >= in[i-1] {
-			result++
-		}
+// Part 2
+func DoTwo(in []int) int {
+	var v []int
+	for i := 0; i < len(in)-2; i++ {
+		x, y, z := in[i], in[i+1], in[i+2]
+		sum := x + y + z
+		// fmt.Println(x, y, z, sum)
+		v = append(v, sum)
 	}
-	return result
+	return countIncreases(v)
 }
--- 2021/01/main_test.go
+++ 2021/01/main_test.go
@@ -26,3 +26,25 @@ func TestDoOne(t *testing.T) {
 		})
 	}
 }
+
+func TestDoTwo(t *testing.T) {
+	tt := []struct {
+		In     []int
+		Result int
+	}{
+		{
+			In:     []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263},
+			Result: 5,
+		},
+	}
+
+	for i, test := range tt {
+		testName := fmt.Sprintf("%v", i)
+		t.Run(testName, func(t *testing.T) {
+			result := DoTwo(test.In)
+			if result != test.Result {
+				t.Errorf("Invalid result. Expected: %v Got: %v", test.Result, result)
+			}
+		})
+	}
+}
