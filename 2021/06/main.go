36e39d3b19c9d0c3a9258434976d520c575183db
Daniel Dides <danieldides@gmail.com>
2021-12-16 10:00:33 -0600

    WIP

--- 2021/06/main.go
+++ 2021/06/main.go
@@ -45,20 +45,29 @@ func DoOne(fish []int) int {
 	return len(fish)
 }
 
-func DoTwo(fish []int) int {
-	for day := 0; day < 256; day++ {
-		for i := 0; i < len(fish); i++ {
-			if fish[i] == 0 {
-				fish = append(fish, 9)
-				fish[i] = 6
-				continue
-			}
-			fish[i]--
-		}
+func sum(m map[int]int) int {
+	var result int
 
-		if day%10 == 0 {
-			fmt.Println(day+1, len(fish))
-		}
+	for _, v := range m {
+		result += v
 	}
+
+	return result
+}
+
+func DoTwo(fish []int) int {
+	// Map of age: number of fish at that age:w
+	ages := [9]int{}
+
+	// Initial population
+	for _, f := range fish {
+		ages[f] += 1
+	}
+
+	for day := 0; day < 18; day++ {
+		fmt.Println("Day: ", day, sum(ages))
+	}
+
+	fmt.Println(ages)
 	return len(fish)
 }
